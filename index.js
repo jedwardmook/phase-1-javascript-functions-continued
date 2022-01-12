16ca3deca80ae788de0c443a487145845d178423
John Mook <jedwardmook@yahoo.com>
2022-01-11 19:54:28 -0600

    commit tree 57d461738ddeace6d7c087a7e24fd8553dd954b6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(para1 = "*") {
+    return function(para2= "special"){
+        return `You are ${para1}${para2}${para1}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
+
+
